587a974e746c8c7f89ff0755fa2f2c6cac9b092e
DivyaK999 <divyadk1231@gmail.com>
2023-06-23 04:28:09 +0000

    DevA comment added

--- demo.java
+++ demo.java
@@ -2,6 +2,7 @@
 public class Demo{
 	public static void main(String args[])
 	{
+		//DevA comment added
 		//print statement for HellowWorld
 		System.out.println("x");
 		// print statement for devops
