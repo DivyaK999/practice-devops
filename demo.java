0a3e9d81b6573b0ec1f8ff599c2c9f2ede82e19a
DivyaK999 <divyadk1231@gmail.com>
2023-06-23 03:53:49 +0000

    Devops changes committed

--- demo.java
+++ demo.java
@@ -1,2 +1,11 @@
 
+public class Demo{
+	public static void main(String args[])
+	{
+		//print statement for HellowWorld
+		System.out.println("x");
+		// print statement for devops
+		System.out.println("y");
+	}
 
+}
